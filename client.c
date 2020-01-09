f5003dbe22cd510437ffe634c81469488671c81d
rleongPd5 <rleong00@stuy.edu>
2020-01-08 22:24:32 -0500

    changed char_check's return value

--- client.c
+++ client.c
@@ -19,18 +19,17 @@
   c: a char to check against
   if: c matches char at the beginning of the paragraph, paragraph gets incremented by 1 (character was typed correctly)
   else: nothing happens
-  returns 0 if nothing changed
-  returns 1 if paragraph was changed
+  returns paragraph shifted over one char if c matched the first char of paragraph
+          otherwise returns the unchanged paragraph
 */
-int char_check(char * paragraph, char * c){
-  printf("paragraph: %s| c: %s\n", paragraph, c);
+char * char_check(char * paragraph, char * c){
+  printf("paragraph: %s \n c: %s\n", paragraph, c);
   printf("paragraph[0]: %c | c[0]: %c\n", paragraph[0], c[0]);
   if (c[0] == paragraph[0]){ //correctly typed
     paragraph++;
     printf("updated paragraph: %s\n", paragraph);
-    return 1;
   }
-  return 0;
+  return paragraph;
 }
 int main(){
   int fd;
@@ -49,7 +48,7 @@ int main(){
   while(paragraph){
     fgets(c, 10, stdin);
     c[strlen(c) - 1] = '\0';
-    char_check(paragraph, c);
+    paragraph = char_check(paragraph, c);
       // printf("\033[2J");
       // printf("Updated: %s\n", paragraph);
     printf("You typed: %s\n", c);
